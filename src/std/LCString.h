1d14c4b07c75bcf0fc94ce98309bc89cfd997f81
cyril collineau <simcas@sfr.fr>
2014-11-12 17:13:18 +0100

    Integration into SimCAS project

---
+++ src/std/LCString.h
@@ -0,0 +1,63 @@
+/*******************************************************************************/
+/*  Copyright (C) 2014 The CASLight project                                     */
+/*                                                                             */
+/*  This program is free software; you can redistribute it and/or modify       */
+/*  it under the terms of the GNU General Public License as published by       */
+/*  the Free Software Foundation; either version 2 of the License, or          */
+/*  (at your option) any later version.                                        */
+/*                                                                             */
+/*  This program is distributed in the hope that it will be useful,            */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of             */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              */
+/*  GNU General Public License for more details.                               */
+/*                                                                             */
+/*  You should have received a copy of the GNU General Public License          */
+/*  along with this program; if not, write to the Free Software                */
+/*  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
+/*******************************************************************************/
+
+#pragma once
+#include <string>
+#include <algorithm>
+#include <cstdio>
+
+#ifdef _WIN32
+#define snprintf _snprintf_s
+#endif
+
+class CString: public std::string
+{
+  public:
+    const char * GetBufferPtr() const                { return c_str(); }
+    unsigned GetLength() const                       { return length(); }
+    void Copy( const CString& s )                    { assign( s ); }
+    void Copy( const char * pText, unsigned length ) { assign(pText,length); }
+    void Clear()                                     { clear(); }
+    void Prepend( const CString& s )                 { insert( 0, s ); }
+    void Append( const CString& s )                  { append( s ); }
+    int Compare(  const CString& s ) const           { return compare(s); }
+    CString Right( unsigned n ) const                { CString s; n>length()? s.Copy(*this): s.append( *this, length()-n, n ); return s;  }
+    size_t Search( char c ) const                    { return find(c); }
+    void SetLength( size_t n )                       { resize( n ); }
+    operator const char * () const                   { return GetBufferPtr();  }
+    bool IsEmpty() const                             { return empty(); }
+    char At( unsigned n ) const                      { return at(n); }
+    void ToLower()                                   { std::for_each(begin(),end(),::tolower); }
+    void Set( int i, unsigned base=10 ) 
+#ifdef _WIN32
+        { char buffer[32];  _itoa_s( i, buffer, base ); Copy(buffer); }
+#else
+        { char buffer[32];  snprintf( buffer, sizeof(buffer), (base==16?"%x":(base==10?"%d":"%o")), i ); Copy(buffer); }
+#endif
+    void Set( double v )
+        { char buffer[32];  snprintf( buffer, sizeof(buffer), "%.12g", v );  Copy( buffer );  }
+    
+    //Constructors
+    CString(const char*s):std::string(s)             {}
+    //CString(const CString &s):std::string(s)         {} //implicit
+    CString(const std::string &s):std::string(s)         {} //implicit
+    CString():std::string()                          {}
+    CString(int i, unsigned base=10)    { Set(i,base); }
+    CString(double v)                    { Set(v); }
+};
+
---
+++ src/std/LCVector.h
@@ -0,0 +1,47 @@
+/*******************************************************************************/
+/*  Copyright (C) 2014 The CASLisght project                                      */
+/*                                                                             */
+/*  This program is free software; you can redistribute it and/or modify       */
+/*  it under the terms of the GNU General Public License as published by       */
+/*  the Free Software Foundation; either version 2 of the License, or          */
+/*  (at your option) any later version.                                        */
+/*                                                                             */
+/*  This program is distributed in the hope that it will be useful,            */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of             */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              */
+/*  GNU General Public License for more details.                               */
+/*                                                                             */
+/*  You should have received a copy of the GNU General Public License          */
+/*  along with this program; if not, write to the Free Software                */
+/*  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
+/*******************************************************************************/
+
+#pragma once
+
+#include <vector>
+
+template < class T > class CVector : public std::vector < T >
+{
+  
+public:
+    void	    SetSize( unsigned size )                      { std::vector < T >::resize( size ); }
+    void	    InsertAt( unsigned index, const T e )         { std::vector < T >::insert( std::vector < T >::begin()+index, e ); }
+    void	    InsertAt( unsigned index, const CVector& v  ) { insert( std::vector < T >::begin()+index, v.std::vector < T >::begin(), v.std::vector < T >::end() ); }
+    void	    RemoveAt( unsigned start )                    { std::vector < T >::erase( std::vector < T >::begin()+start ); }
+    void	    RemoveAt( unsigned start, unsigned count  )   { std::vector < T >::erase( std::vector < T >::begin()+start, std::vector < T >::begin()+start+count ); }
+    void	    Append( const CVector& v  )                   { insert( std::vector < T >::end(), v.begin(), v.end() );  }
+    void	    Append( const T e )                           { std::vector < T >::push_back(e);}
+    void	    RemoveAll()                                   { std::vector < T >::clear(); } 
+    void	    DeleteAll()                                   { while(!std::vector < T >::empty()) { delete std::vector < T >::back(); std::vector < T >::pop_back(); } } 
+    void	    Copy( const CVector& v )                      { std::vector < T >::assign( v.begin(), v.end() ); } 
+    void	    SetAt( unsigned index, const T e )            { std::vector < T >::at(index)=e; }
+    const T&	GetAt( unsigned index ) const                 { return std::vector < T >::at(index); }
+    const T*    GetData() const                               { return std::vector < T >::data(); }
+    T*          GetData()                                     { return std::vector < T >::data(); }
+    unsigned	GetSize() const                               { return std::vector < T >::size(); }
+    void        CheckSize( unsigned index )                   { if( index >= GetSize() ) SetSize( index+1 ); }                
+
+};
+
+
+
